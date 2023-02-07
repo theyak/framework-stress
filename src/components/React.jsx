09ada97a70e2174223a60ddf7790353bf850e33c
theyak <yakkyjunk@hotmail.com>
2023-02-06 19:41:09 -0800

    Dang, that's vanilla level

--- src/components/React.jsx
+++ src/components/React.jsx
@@ -6,10 +6,14 @@ const lots = [...Array(30000).keys()];
 let running = true;
 let timeout = null;
 
-const initialList = [];
-for (let i = 0; i < 500; i++) {
-  initialList.push({ name: token(), id: i });
-}
+const getTokenList = () => {
+  const tokens = [];
+  for (let i = 0; i < 500; i++) {
+    tokens.push({ name: token(), id: i });
+  }
+  return tokens;
+};
+const initialList = getTokenList();
 
 const ListingMemoized = React.memo(() => (
   <>
@@ -29,39 +33,34 @@ const Listing = () => (
   </>
 );
 
+const go = (setList, setCounter) => {
+  setList(getTokenList());
+  setCounter((c) => c + 1);
+
+  if (Date.now() < endAt) {
+    timeout = setTimeout(() => go(setList, setCounter), 1);
+  } else {
+    clearTimeout(timeout);
+    timeout = null;
+  }
+};
+
+function onRun(setList, setCounter) {
+  setCounter(0);
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  endAt = Date.now() + 10000;
+  timeout = setTimeout(() => go(setList, setCounter));
+}
+
+
 function App({ longList }) {
   const [list, setList] = useState(initialList);
   const [count, setCounter] = useState(0);
   const [memo, setMemo] = useState(false);
 
-  const onMemo = useCallback((e) => {
-    setMemo(e.target.checked);
-  }, []);
-
-  const onRun = useCallback((e) => {
-    setCounter(0);
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-    endAt = Date.now() + 10000;
-    timeout = setTimeout(go, 1);
-  }, []);
-
-  const go = () => {
-    const newList = [...list];
-    newList.forEach((item, i) => {
-      item.name = token();
-    });
-    setList(newList);
-    setCounter((c) => c + 1);
-
-    if (Date.now() < endAt) {
-      timeout = setTimeout(go, 1);
-    } else {
-      clearTimeout(timeout);
-      timeout = null;
-    }
-  };
+  const onMemo = (e) => setMemo(e.target.checked);
 
   return (
     <>
@@ -74,7 +73,7 @@ function App({ longList }) {
           Use Memo
         </label>
       </div>
-      <button style={{ marginBottom: "1em" }} onClick={onRun}>
+      <button style={{ marginBottom: "1em" }} onClick={() => onRun(setList, setCounter)}>
         Run!
       </button>
       <ul>
